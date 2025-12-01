b8aae223b02fe1d2466d1d43d3cdbd985fdfe039
MZhafran <muhd.zhafran@gmail.com>
2025-12-01 19:17:24 +0800

    change the title

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: "/CPF-Calculator-Growth-Insights/",
   plugins: [react(), tailwindcss()],
 })
